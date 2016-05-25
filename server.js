710751cc363d52796c1eb2dfc7aca9a031e1f88f
Chanh Tran Huu <chanhtranh@HarveyNash.vn.local>
2016-05-25 10:56:06 +0700

    delete templates folder

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var http = require('http');
-
+var ejs = require('ejs');
 var app = express();
+var path = require('path');
+
+//public file in the public_datasource
+app.use('/assets', express.static(__dirname + '/public_datasource/assets'));
+app.use('/css', express.static(__dirname + '/public_datasource/css'));
+app.use('/js', express.static(__dirname + '/public_datasource/js'));
+app.use('/img', express.static(__dirname + '/public_datasource/img'));
+app.set('views', path.join(__dirname, 'view')); 
+app.set('view engine', 'ejs'); 
+
 
 //import dependency injection
 var di = require('./config/config');
@@ -11,6 +21,10 @@ db.connect();
 app.get('/start', function(req,res){
     res.end('<h1 style="color: red; text-align: center; margin: 200px auto 0px;">TEAM LAY - CHALLENGE2 - KICK OFF 24/5/2016-7/6/2016</h1>');
 });
+
+app.get('/ci',function(req,res){
+    res.render('pages/contact_info');
+})
 
 http.createServer(app).listen(8080,function(){
     console.log("let's read first");
