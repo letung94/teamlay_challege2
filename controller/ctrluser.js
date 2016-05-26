cf620a4e02427230d7147c70b96f29d9f20affce
Nhieu Nguyen Van <nhieunguyenv@HarveyNash.vn.local>
2016-05-26 10:17:56 +0700

    Nhieu commit router,db

--- controller/ctrluser.js
+++ controller/ctrluser.js
@@ -1,71 +1,24 @@
 /*----------This is Nhieu's code----------*/
-function ProcessDBUserMysql(connectiondb) {
-    this.connectionmysql = connectiondb;
-};
+var express=require('express');
+var bodyparser=require('body-parser');
+var jsonparser=bodyparser.json();
+var router=express.Router();
+var di = require('../config/config');
+var db = di.resolve('db');
 
-ProcessDBUserMysql.prototype.readcurriculumvitaebyid = function (req, callback) {
-    console.log("processing");
-    // console.log(this.connectionmysql.host,
-    //     this.connectionmysql.user,
-    //     this.connectionmysql.pass,
-    //     this.connectionmysql.dbname    );
-    var conn = this.connectionmysql.mysql.createConnection({
-        host: this.connectionmysql.host,
-        user: this.connectionmysql.user,
-        password: this.connectionmysql.pass,
-        database: this.connectionmysql.dbname
-    });
-    //console.log(conn);
-    if (conn) {
-        try {
-            conn.connect();
-            var idcv = req.params.idcv;
-            conn.query('SELECT * FROM curriculum_vitae WHERE id=?', [idcv], function (err, rows, fields) {
-                var data = {};
-                if (!err) {
-                    data = rows[0].firstname;
-                }
-                else {
-                    data = false;
-                    console.log("There is error");
-                }
-                callback(rows[0]);
-                // callback(data);
-                conn.end();
-                console.log("the");
-            });
-        }
-        catch (err) {
-            console.log(err);
-        }
-    }
-};
+router.get('/cv/:idcv',function(req,res){
+    db.readCVById(req,function(flag,data){
+        res.end(JSON.stringify(data));    
+    });        
+});
 
-module.exports = ProcessDBUserMysql;
+router.post('/cv/:idcv/contact-info/update',[jsonparser],function(req,res){
+    db.updateContactInfo(req,function(flag,data){
+        res.json(data);
+        res.end();            
+    });    
+});
 
-// exports.updatecontactinfo = function (req, res) {
-//     if (!req.body) return res.sendStatus(400);
-//     var firstname = req.body.firstname;
-//     var lastname = req.body.lastname;
-//     var email = req.body.email;
-//     var phone = req.body.phone;
-//     var website = req.body.website;
-//     var address = req.body.address;
-//     var idcv = req.params.idcv;
-//     // connection.connect();
-//     req.getConnection(function (err, connection) {
+module.exports = router;
 
-//         var query = connection.query('SELECT * FROM curriculum_vitae', function (err, rows) {
-
-//             if (err)
-//                 console.log("Error Selecting : %s ", err);
-
-//             res.end(JSON.stringify(rows[0]));
-
-
-//         });
-
-//         //console.log(query.sql);
-//     });
-// };
-// /*--------------------*/
+/*--------------------*/
