7ec0935158d319d185ddbc3def056df243e00b23
Phat Tran Tan <phattrant@HarveyNash.vn.local>
2016-06-01 10:21:45 +0700

    commit fix summary load data

--- public_datasource/js/summary.js
+++ public_datasource/js/summary.js
@@ -13,7 +13,8 @@ function getSummary(){
         async: false,
         contentType: 'application/json; charset=utf-8',
         //json object to sent to the authentication url
-        success: function (res) {   
+        success: function (res) {  
+                clickedSummary = true; 
                 $("input[name='headline']").val(res.resdata.Headline);
                 $("textarea[name='prosummary']").data('wysihtml5').editor.setValue(res.resdata.ProfessionalSummary);
                 },
