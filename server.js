5193ee9d89b29d8ac2cf8caa23a36ec8be8c11d4
unknown <duybui.hcmit@outlook.com>
2016-06-02 00:16:01 +0700

    Authentication (Login, Register, Forgot pass, Email verification ) done! --> move to Testing

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mailer.extend(app, {
     port: 465, // port for secure SMTP
     transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
     auth: {
-        user: 'duybui.hcmit',
+        user: 'hvn.cvmaker',
         pass: '01269848891'
     }
 });
