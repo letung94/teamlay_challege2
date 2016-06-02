356b2ea93fdb1d44f60352c388f396ceb20ca56b
saboteur94 <saboteur94@yahoo.com>
2016-06-02 21:27:50 +0700

    + Add Certificate's server-side validation.
    + Redirect to Preview CV page while Editing CV by clicking at " Preview & Download " button.

--- model/certification.js
+++ model/certification.js
@@ -1,11 +1,69 @@
-function certificationModel() {
-    // console.log('in');
-    var Certification = require('../config/config').resolve("db").Certification;
+function certificationModel(title, certificateAuthority, date, details, CV_Id) {
+    var self = this;
+    self.attribute = {
+        "Title" : title,
+        "CertificateAuthority" : certificateAuthority,
+        "Date" : date,
+        "Details" : details,
+        "CV_Id" : CV_Id
+    };
 
-    this.getAllCertificationByCVId = function (cv_id, callback) { // param: CV_Id,
-        certification = new Certification();
+    self.attrvalidate = [
+        {
+            attrname: 'Title',
+            validate: function(title){
+                var valid = true;
+                var max = 100;
+                if(!title || title == ''){
+                    valid = false;
+                }
+                if(title.length > 100){
+                    valid = false;
+                }
+                return valid;
+            }
+        },
+        {
+            attrname: 'Date',
+            validate: function(date){
+                /*Validate smaller than today*/
+                var valid = true;
+                if(date && date != null){
+                    var today = new Date();
+                    var date = new Date(date);
+                    valid = date <= today;
+                }
+                return valid;
+            }
+        },
+        {
+            attrname: 'CV_Id',
+            validate: function(CV_Id){
+                /*CV_Id is require and Larger than 0 */
+                var valid = true;
+                if(!CV_Id || CV_Id <= 0){
+                    valid = false;
+                }
+                return valid;
+            }
+        },
+    ];
+
+    self.checkValidation = function(){
+        var valid = true;
+        var attr_length = self.attrvalidate.length;
+        for (var i = 0; i < attr_length; i++) {
+            if (self.attrvalidate[i].validate != null) {
+                valid &= self.attrvalidate[i].validate(self.attribute[self.attrvalidate[i].attrname]);
+            }
+        }
+        return valid;
+    }
+
+    self.Certification = require('../config/config').resolve("db").Certification;
+    self.getAllCertificationByCVId = function (cv_id, callback) { // param: CV_Id,
+        var certification = new self.Certification();
         certification.find('all',{where:'CV_Id=' + cv_id},function(err,rows,fields){
-            certification.killConnection();
             if(err){
                 callback(-1, err)
             }else{
@@ -18,10 +76,9 @@ function certificationModel() {
         });
     }
 
-    this.saveCertification = function(param, callback){
-        certification = new Certification(param);
+    self.saveCertification = function(param, callback){
+        certification = new self.Certification(param);
         certification.save(function(errors, results){
-            certification.killConnection();
             if(errors){
                 callback(-1, errors)
             }else{
@@ -34,10 +91,9 @@ function certificationModel() {
         });
     }
 
-    this.deleteCertification = function(id, callback){
-        certification = new Certification({id: id});
+    self.deleteCertification = function(id, callback){
+        certification = new self.Certification({id: id});
         certification.remove(function(err, rows){
-            certification.killConnection();
             if (err) {
                 callback(-1, err)
             } else {
