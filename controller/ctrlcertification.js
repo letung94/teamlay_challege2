3e340ec66bd4b9134c5ac4d8967e2143363320dc
saboteur94 <saboteur94@yahoo.com>
2016-06-04 10:11:09 +0700

    + Certificate Controller: Validate at Service instead of controller, change from code to flag.
    + Certificate (Client): Check flag instead of code.
    + Certificate (View): Add red star and node for mandatory field.
    + Certificate (Controller) + Footer: Update the reference link to prevent case sensitive when deploy into linux.

--- controller/ctrlcertification.js
+++ controller/ctrlcertification.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var Certification = require('../model/Certification');
 var di = require('../config/config');
 
 module.exports = router;
@@ -10,19 +9,19 @@ router.get('/certification/getAll', function (req, res) {
 	var userId = req.user.Id;
 	var cvService = di.resolve('curriculum_vitae');
 	var cvServiceIns = new cvService();
-	cvServiceIns.checkCVBelongToUser(cv_id, userId, function(code, data){
-		if(code == 1){
+	cvServiceIns.checkCVBelongToUser(cv_id, userId, function(flag, data){
+		if(flag == 1){
 			if(data == true){ /*This cv_id belong to this user*/
 				var cerService = di.resolve('certification');
 				var cerServiceIns = new cerService();
-				cerServiceIns.getAllCertificationByCVId(cv_id, function(cerCode, rows){
-					return res.json({code: cerCode, rows: rows});
+				cerServiceIns.getAllCertificationByCVId(cv_id, function(cerFlag, rows){
+					return res.json({flag: cerFlag, rows: rows});
 				});
 			}else{/*This cv_id not belong to this user*/
-				return res.json({code : code, msg: 'The CV you send belong to other user.'} );
+				return res.json({flag : flag, msg: 'The CV you send belong to other user.'} );
 			}
-		}else if (code == -1){ /*Somethong wrong with server*/
-			return res.json({code: code, msg:''});
+		}else if (flag == -1){ /*Somethong wrong with server*/
+			return res.json({flag: flag, msg:''});
 		}
 	})
 });
@@ -30,26 +29,13 @@ router.get('/certification/getAll', function (req, res) {
 router.post('/certification/add', function (req, res) {
 	var idcv = req.baseUrl.split("/")[2];
 	var entity = req.body.entity;
-	var certification = new Certification(
-		entity.Title,
-		entity.CertificateAuthority,
-		entity.Date,
-		entity.Details,
-		idcv
-	);
-	var valid = certification.checkValidation();
-	if(valid)
-	{
-		var cerService = di.resolve('certification');
-		var cerServiceIns = new cerService();
+	var cerService = di.resolve('certification');
+	var cerServiceIns = new cerService();
 
-		entity.CV_Id = idcv;
-		cerServiceIns.saveCertification(entity, function(code, data){
-			res.json({code: code, data: data});
-		})
-	}else{
-		res.json({code: 0});
-	}
+	entity.CV_Id = idcv;
+	cerServiceIns.saveCertification(entity, function(flag, data){
+		res.json({flag: flag, data: data});
+	})
 });
 
 router.post('/certification/edit', function (req, res) {
@@ -57,20 +43,21 @@ router.post('/certification/edit', function (req, res) {
 	var userId = req.user.Id;
 	var cvService = di.resolve('curriculum_vitae');
 	var cvServiceIns = new cvService();
-	cvServiceIns.checkCVBelongToUser(cv_id, userId, function(code, data){
-		if(code == 1){
+	cvServiceIns.checkCVBelongToUser(cv_id, userId, function(flag, data){
+		if(flag == 1){
 			if(data == true){ /*This cv_id belong to this user*/
 				var cerService = di.resolve('certification');
 				var cerServiceIns = new cerService();
 				var entity = req.body.entity;
-				cerServiceIns.saveCertification(entity, function(cerCode, rows){
-					return res.json({code: cerCode, rows: rows});
+				entity.CV_Id = cv_id;
+				cerServiceIns.saveCertification(entity, function(cerFlag, rows){
+					return res.json({flag: cerFlag, rows: rows});
 				})
 			}else{/*This cv_id not belong to this user*/
-				return res.json({code : code, msg: 'The CV you want to edit belong to another user.'} );
+				return res.json({flag : cerFlag, msg: 'The CV you want to edit belong to another user.'} );
 			}
-		}else if (code == -1){ /*Something wrong with server*/
-			return res.json({code: code});
+		}else if (flag == -1){ /*Something wrong with server*/
+			return res.json({flag: flag});
 		}
 	});
 });
@@ -80,20 +67,20 @@ router.post('/certification/delete', function (req, res) {
 	var userId = req.user.Id;
 	var cvService = di.resolve('curriculum_vitae');
 	var cvServiceIns = new cvService();
-	cvServiceIns.checkCVBelongToUser(cv_id, userId, function(code, data){
-		if(code == 1){
+	cvServiceIns.checkCVBelongToUser(cv_id, userId, function(flag, data){
+		if(flag == 1){
 			if(data == true){ /*This cv_id belong to this user*/
 				var cerService = di.resolve('certification');
 				var cerServiceIns = new cerService();
 				var certificationId = req.body.id;
-				cerServiceIns.deleteCertification(certificationId, function(cerCode, rows){
-					return res.json({code: cerCode, rows: rows});
+				cerServiceIns.deleteCertification(certificationId, function(cerFlag, rows){
+					return res.json({flag: cerFlag, rows: rows});
 				})
 			}else{/*This cv_id not belong to this user*/
-				return res.json({code : code, msg: 'The CV you want to delete belong to another user.'} );
+				return res.json({flag : cerFlag, msg: 'The CV you want to delete belong to another user.'} );
 			}
-		}else if (code == -1){ /*Somethong wrong with server*/
-			return res.json({code: code});
+		}else if (flag == -1){ /*Somethong wrong with server*/
+			return res.json({flag: flag});
 		}
 	});
 });
