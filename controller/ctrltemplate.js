8046279a72c63712a51ee33f568f86a6d983de2e
saboteur94 <saboteur94@yahoo.com>
2016-06-07 17:05:08 +0700

    + Update Template Check User by Id.

--- controller/ctrltemplate.js
+++ controller/ctrltemplate.js
@@ -11,142 +11,165 @@ var uuid = require('node-uuid');
 router.get('/templateReview/:name/:cv_id', function (req, res) {
 	var name = req.params.name;
 	var cv_id = req.params.cv_id;
-	var info = {};
-	async.waterfall([
-		function(done){
-			/*Contact Info*/
-			var ContactInfoService = di.resolve('contact_info');
-			var contactInfoService = new ContactInfoService();
-			contactInfoService.getByIdCV({CV_Id: cv_id}, function(code, data){
-				info.contact_info = data;
-				done();
-			})
-		},
-		function(done){
-			/*Summary*/
-			var SummaryService = di.resolve('summary');
-			var summaryService = new SummaryService();
-			summaryService.getByIdCV({CV_Id: cv_id}, function(code, data){
-				info.summary = data;
-				done();
-			})
-		},
-		function(done){
-			/*Experience*/
-			var ExperienceService = di.resolve('experience');
-			var experienceService = new ExperienceService();
-			experienceService.getAllExperienceByCVId( cv_id, function(code, data){
-				info.experiences = data;
-				done();
-			})
-		},
-		function(done){
-			/*Skill*/
-			var SkillService = di.resolve('skill');
-			var skillService = new SkillService();
-			skillService.getAllSkillByCVId(cv_id, function(code, data){
-				info.skills = data;
-				done();
-			})
-		},
-		function(done){
-			/*Education*/
-			var EducationService = di.resolve('education');
-			var educationService = new EducationService();
-			educationService.getAllByIdCV(cv_id, function(code, data){
-				if(code == 1){
-					/*Convert DB date to readable date*/
-					if(data){
-						var length = data.length;
-						for (var i = 0; i < length; i++) {
-							var education = data[i];
-							if(education.FromDate){
-								education.FromDate = helper.parseDate(education.FromDate);
+	var userid = req.user.Id;
+	cvServiceIns.checkCVBelongToUser(cv_id, userid, function(flag, data) {
+		if (flag == 1) {
+			if (data == true) { /*This cv_id belong to this user*/
+				var info = {};
+				async.waterfall([
+					function(done){
+						/*Contact Info*/
+						var ContactInfoService = di.resolve('contact_info');
+						var contactInfoService = new ContactInfoService();
+						contactInfoService.getByIdCV({CV_Id: cv_id}, function(code, data){
+							info.contact_info = data;
+							done();
+						})
+					},
+					function(done){
+						/*Summary*/
+						var SummaryService = di.resolve('summary');
+						var summaryService = new SummaryService();
+						summaryService.getByIdCV({CV_Id: cv_id}, function(code, data){
+							info.summary = data;
+							done();
+						})
+					},
+					function(done){
+						/*Experience*/
+						var ExperienceService = di.resolve('experience');
+						var experienceService = new ExperienceService();
+						experienceService.getAllExperienceByCVId( cv_id, function(code, data){
+							info.experiences = data;
+							done();
+						})
+					},
+					function(done){
+						/*Skill*/
+						var SkillService = di.resolve('skill');
+						var skillService = new SkillService();
+						skillService.getAllSkillByCVId(cv_id, function(code, data){
+							info.skills = data;
+							done();
+						})
+					},
+					function(done){
+						/*Education*/
+						var EducationService = di.resolve('education');
+						var educationService = new EducationService();
+						educationService.getAllByIdCV(cv_id, function(code, data){
+							if(code == 1){
+								/*Convert DB date to readable date*/
+								if(data){
+									var length = data.length;
+									for (var i = 0; i < length; i++) {
+										var education = data[i];
+										if(education.FromDate){
+											education.FromDate = helper.parseDate(education.FromDate);
+										}
+										if(education.ToDate){
+											education.ToDate = helper.parseDate(education.ToDate);
+										}
+									}
+									info.educations = data;
+								}
 							}
-							if(education.ToDate){
-								education.ToDate = helper.parseDate(education.ToDate);
+							done();
+						})
+					},
+					function(done){
+						/*Education*/
+						var ProjectService = di.resolve('project');
+						var projectService = new ProjectService();
+						projectService.getAllProjectByCVId({CV_Id: cv_id}, function(code, data){
+							if(code == 1){
+								/*Convert DB date to readable date*/
+								if(data){
+									var length = data.length;
+									for (var i = 0; i < length; i++) {
+										var project = data[i];
+										if(project.FromDate){
+											project.FromDate = helper.parseDate(project.FromDate);
+										}
+										if(project.ToDate){
+											project.ToDate = helper.parseDate(project.ToDate);
+										}
+									}
+									info.projects = data;
+								}else{
+									info.projects = [];
+								}
 							}
-						}
-						info.educations = data;
-					}
-				}
-				done();
-			})
-		},
-		function(done){
-			/*Education*/
-			var ProjectService = di.resolve('project');
-			var projectService = new ProjectService();
-			projectService.getAllProjectByCVId({CV_Id: cv_id}, function(code, data){
-				if(code == 1){
-					/*Convert DB date to readable date*/
-					if(data){
-						var length = data.length;
-						for (var i = 0; i < length; i++) {
-							var project = data[i];
-							if(project.FromDate){
-								project.FromDate = helper.parseDate(project.FromDate);
+							done();
+						})
+					},
+					function(done){
+						/*Certificate*/
+						var CertificateionService = di.resolve('certification');
+						var certificateionService = new CertificateionService();
+						certificateionService.getAllCertificationByCVId(cv_id, function(code, data){
+							if(code == 1){
+								/*Convert DB date to readable date*/
+								var length = data.length;
+								for (var i = 0; i < length; i++) {
+									var certification = data[i];
+									if(certification.Date){
+										certification.Date = helper.parseDate(certification.Date);
+									}
+								}
+								info.certifications = data;
 							}
-							if(project.ToDate){
-								project.ToDate = helper.parseDate(project.ToDate);
+							done();
+						});
+					},
+					function(done){
+						var templatePath = path.join(__dirname + '/../view/templates/' + name);
+						fs.exists(templatePath, function (exist) {
+							if(exist){
+								res.render('templates/' + name, info);
+							}else{
+								res.render('pages/generic_error', {ExcludeHeader: '1', Title:'CV Template not found',Code: '404',
+								Detail: 'The template you given is not exist or under maintenance, please try again later.'});
 							}
-						}
-						info.projects = data;
-					}else{
-						info.projects = [];
-					}
-				}
-				done();
-			})
-		},
-		function(done){
-			/*Certificate*/
-			var CertificateionService = di.resolve('certification');
-			var certificateionService = new CertificateionService();
-			certificateionService.getAllCertificationByCVId(cv_id, function(code, data){
-				if(code == 1){
-					/*Convert DB date to readable date*/
-					var length = data.length;
-					for (var i = 0; i < length; i++) {
-						var certification = data[i];
-						if(certification.Date){
-							certification.Date = helper.parseDate(certification.Date);
-						}
-					}
-					info.certifications = data;
-				}
-				done();
-			});
-		},
-		function(done){
-			var templatePath = path.join(__dirname + '/../view/templates/' + name);
-			fs.exists(templatePath, function (exist) {
-				if(exist){
-					res.render('templates/' + name, info);
-				}else{
-					res.render('pages/generic_error', {ExcludeHeader: '1', Title:'CV Template not found',Code: '404',
-					Detail: 'The template you given is not exist or under maintenance, please try again later.'});
-				}
-			});
-		},
-	], function(err){
-		res.render('pages/server_error_505');
-	});
+						});
+					},
+				], function(err){
+					res.render('pages/server_error_505');
+				});
+			} else { /*This cv_id not belong to this user*/
+				res.render('pages/server_error_500')
+			}
+		} else  { /*Somethong wrong with server*/
+			res.render('pages/server_error_500')
+		}
+	})
 });
 
 router.get('/template_list/:cv_id', function (req, res) {
-	files = [];
-	var dir = __dirname + '/../view/templates';
-	fs.readdir(dir, function (err, filesFromDisk) {
-		if (err) { return; }
-		filesFromDisk.forEach(function (f) {
-			var name = dir + '/' + f;
-			if (!fs.statSync(name).isDirectory()) {
-				files.push(f);
+	var cv_id = req.params.cv_id;
+	var userid = req.user.Id;
+	cvServiceIns.checkCVBelongToUser(cv_id, userid, function(flag, data) {
+		if (flag == 1) {
+			if (data == true) { /*This cv_id belong to this user*/
+				files = [];
+				var dir = __dirname + '/../view/templates';
+				fs.readdir(dir, function (err, filesFromDisk) {
+					if (err) { return; }
+					filesFromDisk.forEach(function (f) {
+						var name = dir + '/' + f;
+						if (!fs.statSync(name).isDirectory()) {
+							files.push(f);
+						}
+					});
+					res.render('pages/template_list', { files: files });
+				});
+			} else { /*This cv_id not belong to this user*/
+				res.render('pages/server_error_500')
 			}
-		});
-		res.render('pages/template_list', { files: files });
-	});
+		} else { /*Somethong wrong with server*/
+			res.render('pages/server_error_500')
+		}
+	})
 });
 
 router.get('/download/:name/:cv_id', function (req, res) {
